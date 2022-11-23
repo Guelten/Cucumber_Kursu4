fce63b7acd384db91295cb7c60af648d8fe94c73
gcamp <g.campinari@hotmail.com>
2022-11-23 10:37:01 +0100

    Framework hazirlik 2

--- src/test/java/StepDefinitions/_01_LoginSteps.java
+++ src/test/java/StepDefinitions/_01_LoginSteps.java
@@ -1,19 +1,43 @@
 package StepDefinitions;
 
+import Pages.DialogContent;
+import Utilities.GWD;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.testng.Assert;
+
+import java.time.Duration;
 
 public class _01_LoginSteps {
+    DialogContent dc=new DialogContent();
+
     @Given("Naviagate to Campus")
     public void naviagateToCampus() {
+        GWD.getDriver().get("https://demo.mersys.io/");
+        GWD.getDriver().manage().window().maximize();
+
     }
 
     @When("Enter username and password and click Login button")
     public void enterUsernameAndPasswordAndClickLoginButton() {
+
+        WebDriverWait wait = new WebDriverWait(GWD.getDriver(), Duration.ofSeconds(30));
+        wait.until(ExpectedConditions.visibilityOf(dc.username));
+
+        dc.username.sendKeys("richfield.edu");
+        dc.password.sendKeys(" Richfield2020!");
+        dc.loginButton.click();
     }
 
+
     @Then("User should login successfuly")
-    public void userShouldLoginSuccessfuly() {
+    public void userShouldLoginSuccessfuly()
+    {
+        Assert.fail();
     }
+
 }
