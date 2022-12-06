a5d5b0a8ed2df086e96e57fa2eaf24e5430db43f
gcamp <g.campinari@hotmail.com>
2022-12-06 10:15:32 +0100

    Extend Report (Screenshot, extent-config.xml , Sistem info settings, PDF-Report)

--- src/test/java/Runners/_07_TestRunnerExtentReport.java
+++ src/test/java/Runners/_07_TestRunnerExtentReport.java
@@ -1,7 +1,9 @@
 package Runners;
 
+import com.aventstack.extentreports.service.ExtentService;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
+import org.junit.AfterClass;
 
 @CucumberOptions(
         tags = "@Regression",
@@ -11,4 +13,19 @@ import io.cucumber.testng.CucumberOptions;
 )
 
 public class _07_TestRunnerExtentReport extends AbstractTestNGCucumberTests {
+
+
+    @AfterClass // rapor bittiği zaman // altaki bilgilere eklemeler veya cikartmalar yapilabilir.
+    public static void writeExtentReport() {
+        ExtentService.getInstance().setSystemInfo("Windows User Name", System.getProperty("user.name"));
+        ExtentService.getInstance().setSystemInfo("Time Zone", System.getProperty("user.timezone"));
+        ExtentService.getInstance().setSystemInfo("User Name 2", "Gülten Campinari");
+        ExtentService.getInstance().setSystemInfo("Application Name", "Campus");
+        ExtentService.getInstance().setSystemInfo("Operating System Info", System.getProperty("os.name"));
+        ExtentService.getInstance().setSystemInfo("Department", "QA");
+        ExtentService.getInstance().setSystemInfo("Ek Satır", "Açıklama");
+    }
 }
+
+// https://docs.oracle.com/javase/tutorial/essential/environment/sysprop.html
+// bu sayfadan daha fazla "getProperty" ler alinabilir.
